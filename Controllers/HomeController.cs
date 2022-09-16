bab3913649fcd60820211b5d5af419a1601fbc84
Melis Derveni <melisderveni@yahoo.com>
2022-09-16 18:48:19 +0200

    Day1

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Projekti.Models;
+namespace Monsters.Controllers;
+    
+public class HomeController : Controller
+{
+    private MyContext _context;
+    
+    
+    public HomeController(MyContext context)
+    {
+        _context = context;
+    }
+    
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        
+
+        return View();
+    }
+}
+
---
+++ wwwroot/js/site.js
@@ -0,0 +1,24 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+window.addEventListener('scroll', reveal);
+
+function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
+
+    for(var i=0; i < reveals.length; i++)
+    {
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+        if(revealtop < windowheight - revealpoint)
+        {
+            reveals[i].classList.add('active');
+        }
+        else
+        {
+            reveals[i].classList.remove('active');
+        }
+    }
+}
